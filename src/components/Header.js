4632e3bd47d242aa1ed698103ca0b599f4d17461
Denisa Kita <denisakita7@gmail.com>
2024-01-10 16:09:33 +0100

    navigation structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import {Link, NavLink} from "react-router-dom";
 import Logo from "../assets/logo192.png";
+import {useState} from "react";
 
 export const Header = () => {
+    const [hidden, setHidden] = useState(true);
+
+    const activeClass = "text-base block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white";
+    const inactiveClass = "text-base block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
+
     return (
         <header>
             <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
@@ -9,10 +15,12 @@ export const Header = () => {
                     <Link to="/" className="flex items-center">
                         <img src={Logo} className="mr-3 h-6 sm:h-9" alt="Cinemate Logo"/>
                         <span
-                            className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Cinemate</span>
+                            className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Movie Future</span>
                     </Link>
                     <div className="flex md:order-2">
-                        <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
+
+                        <button onClick={() => setHidden(!hidden)} type="button" data-collapse-toggle="navbar-search"
+                                aria-controls="navbar-search"
                                 aria-expanded="false"
                                 className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
                             <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
@@ -23,6 +31,7 @@ export const Header = () => {
                             </svg>
                             <span className="sr-only">Search</span>
                         </button>
+
                         <div className="hidden relative md:block">
                             <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                                 <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor"
@@ -35,9 +44,10 @@ export const Header = () => {
                             </div>
                             <input type="text" id="search-navbar"
                                    className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                   placeholder="Search..."/>
+                                   placeholder="Search..." autoComplete="off"/>
                         </div>
-                        <button data-collapse-toggle="navbar-search" type="button"
+
+                        <button onClick={() => setHidden(!hidden)} data-collapse-toggle="navbar-search" type="button"
                                 className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                                 aria-controls="navbar-search" aria-expanded="false">
                             <span className="sr-only">Open menu</span>
@@ -49,8 +59,9 @@ export const Header = () => {
                             </svg>
                         </button>
                     </div>
-                    <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
-                         id="navbar-search">
+                    <div
+                        className={`${hidden ? "hidden" : ""} justify-between items-center w-full md:flex md:w-auto md:order-1`}
+                        id="navbar-search">
                         <div className="relative mt-3 md:hidden">
                             <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                                 <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor"
@@ -62,26 +73,25 @@ export const Header = () => {
                             </div>
                             <input type="text" id="search-navbar"
                                    className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                   placeholder="Search..."/>
+                                   placeholder="Search..." autoComplete="off"/>
                         </div>
                         <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
-                                <NavLink to="/"
-                                         className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                                <NavLink to="/" className={({isActive}) => isActive ? activeClass : inactiveClass}
                                          end>Home</NavLink>
                             </li>
                             <li>
                                 <NavLink to="/movies/popular"
-                                         className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Popular</NavLink>
+                                         className={({isActive}) => isActive ? activeClass : inactiveClass}>Popular</NavLink>
                             </li>
                             <li>
                                 <NavLink to="/movies/top"
-                                         className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Top
+                                         className={({isActive}) => isActive ? activeClass : inactiveClass}>Top
                                     Rated</NavLink>
                             </li>
                             <li>
                                 <NavLink to="/movies/upcoming"
-                                         className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Upcoming</NavLink>
+                                         className={({isActive}) => isActive ? activeClass : inactiveClass}>Upcoming</NavLink>
                             </li>
                         </ul>
                     </div>
