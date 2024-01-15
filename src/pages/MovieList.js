0cabbb5839c510e7a2ce1947839ebc21981cbd14
Denisa Kita <denisakita7@gmail.com>
2024-01-15 22:48:04 +0100

    movie api

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,11 +1,27 @@
 import {Card} from "../components";
+import {useEffect, useState} from "react";
 
 export const MovieList = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchMovies() {
+            const response = await fetch('https://swapi.dev/api/films/');
+            const data = await response.json();
+            setMovies(data.results)
+        }
+
+        fetchMovies();
+    }, [])
+
     return (
         <main>
             <section className="max-w-7xl mx-auto py-7">
                 <div className="flex justify-start flex-wrap">
-                    <Card/>
+                    {movies && movies.map((movie) => (
+                        <Card key={movie.id} movie={movie}/>
+                    ))}
+
                 </div>
             </section>
         </main>
