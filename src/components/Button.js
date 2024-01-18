f9dbaf869b961b4a34ab2db96c081b1f36172755
Denisa Kita <denisakita7@gmail.com>
2024-01-18 13:43:11 +0100

    page not found

---
+++ src/components/Button.js
@@ -0,0 +1,6 @@
+export const Button = ({children}) => {
+  return(
+      <button className="w-64 h-30 rounded-lg text-white bg-gray-900 p-2">{children}</button>
+  )
+}
+
--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,11 +1,18 @@
 import PageNotFoundImage from "../assets/images/pagenotfound.jpg";
+import {Link} from "react-router-dom";
+import {Button} from "../components";
 
 export const PageNotFound = () => {
+
     return (
         <main>
             <section className="flex flex-col items-center justify-center h-screen">
                 <div className="flex justify-center my-4 max-w-lg">
-                    <img className="w-50px" src={PageNotFoundImage} alt="Page Not Found" />
+                    <img className="w-50px" src={PageNotFoundImage} alt="Page Not Found"/>
+                </div>
+                <div className=" flex justify-center py-6">
+                    <Link to="/"></Link>
+                    <Button>Back to Movie Future</Button>
                 </div>
             </section>
         </main>
